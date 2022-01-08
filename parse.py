e18972ab504e41172ee7b18bbfc2eb34f73c22fa
enmanuelmag <enmanuelmag@cardor.dev>
2022-01-08 09:13:39 -0500

    Update

--- parse.py
+++ parse.py
@@ -7,6 +7,10 @@ def get_filenames(path):
   filenames = []
   for filename in os.listdir(path):
     if filename.endswith(".txt"):
+      exits_image = os.path.isfile(os.path.join(
+        path.replace('labels', 'images'),
+        filename.replace('txt', 'png')
+      ))
       filenames.append(filename)
   return filenames
 
@@ -19,12 +23,17 @@ def save_csv(path, save_path_train, save_path_test):
 
   train_filenames = filenames[:int(length*0.8)]
   test_filenames = filenames[int(length*0.8):]
-  
+  #delete old csv
+  if os.path.isfile(save_path_train):
+    os.remove(save_path_train)
+  if os.path.isfile(save_path_test):
+    os.remove(save_path_test)
+    
   with open(save_path_test, 'w') as csvfile:
     for filename in train_filenames:
-      csvfile.write(str(filename) + ',' +str(filename) + '\n')
+      csvfile.write(str(filename).replace('txt', 'png') + ',' +str(filename) + '\n')
   with open(save_path_train, 'w') as csvfile:
     for filename in test_filenames:
-      csvfile.write(str(filename) + ',' +str(filename) + '\n')
+      csvfile.write(str(filename).replace('txt', 'png') + ',' +str(filename) + '\n')
 
 save_csv('./USD_DIVISA/labels', './USD_DIVISA/train.csv', './USD_DIVISA/test.csv')
