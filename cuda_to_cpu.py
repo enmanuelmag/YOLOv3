1ff6fd97b9b576a0754794e7c2b757ae02c1f2f8
enmanuelmag <enmanuelmag@cardor.dev>
2022-01-09 16:14:39 -0500

    Add new pkls

---
+++ cuda_to_cpu.py
@@ -0,0 +1,15 @@
+import pickle as pkl
+import pandas as pd
+
+loos = pkl.load(open('./loss/20220109_081231_all_looses.pkl', 'rb'))
+metrics = pkl.load(open('./loss/20220109_081231_all_metrics.pkl', 'rb'))
+
+#convert array of dict to dataframe
+loos = pd.DataFrame(loos)
+metrics = pd.DataFrame(metrics)
+
+print(loos.head())
+print(metrics.head())
+
+loos.to_pickle('./data/looses.pkl')
+metrics.to_pickle('./data/metrics.pkl')
